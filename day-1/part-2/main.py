7f80e4fe7f0f479b55467856a7d7048e328cc829
Steve Rydz <steverydz@gmail.com>
2024-12-01 11:00:35 +0000

    Solved second part of day 1

---
+++ day-1/part-2/main.py
@@ -0,0 +1,41 @@
+def get_data():
+    file = open("inputs.txt", "r")
+    content = file.readlines()
+    file.close()
+
+    return content
+
+
+def format_data():
+    data = get_data()
+
+    left = []
+    right = []
+
+    for item in data:
+        items = item.split("   ")
+        left.append(int(items[0]))
+        right.append(int(items[1].rstrip("\n")))
+
+    left.sort()
+    right.sort()
+
+    lists = [left, right]
+
+    return lists
+
+
+def find_number():
+    lists = format_data()
+
+    total = 0
+
+    for item in lists[0]:
+        count = lists[1].count(item)
+        total += item * count
+
+    print(total)
+    return total
+
+
+find_number()
