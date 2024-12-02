cbabf87df41d37cb01684b85c9d27ab804d8fdbc
Steve Rydz <steverydz@gmail.com>
2024-12-02 18:53:34 +0000

    Solution to day 2 part 2

---
+++ day-02/part-2/main.py
@@ -0,0 +1,116 @@
+def get_reports():
+    file = open("inputs.txt", "r")
+    content = file.readlines()
+    file.close()
+
+    reports = list(map(get_levels, content))
+
+    return reports
+
+
+def get_levels(item):
+    record = item.rstrip("\n").split(" ")
+
+    return list(map(int, record))
+
+
+def is_increasing(report):
+    levels = []
+
+    for level in report:
+        if levels and level <= levels[-1]:
+            return False
+
+        levels.append(level)
+
+    return True
+
+
+def is_safe_increasing(report):
+    if not is_increasing(report):
+        return False
+
+    levels = []
+
+    for level in report:
+        if levels:
+            prev = levels[-1]
+
+            if level - prev < 1:
+                return False
+
+            if level - prev > 3:
+                return False
+
+        levels.append(level)
+
+    return True
+
+
+def is_decreasing(report):
+    levels = []
+
+    for level in report:
+        if levels and level >= levels[-1]:
+            return False
+
+        levels.append(level)
+
+    return True
+
+
+def is_safe_decreasing(report):
+    if not is_decreasing(report):
+        return False
+
+    levels = []
+
+    for level in report:
+        if levels:
+            prev = levels[-1]
+
+            if prev - level < 1:
+                return False
+
+            if prev - level > 3:
+                return False
+
+        levels.append(level)
+
+    return True
+
+
+def safe_increasing_dampened_reports(report):
+    for i in range(len(report)):
+        if is_safe_increasing(report[:i] + report[i + 1 :]):
+            return True
+
+    return False
+
+
+def safe_decreasing_dampened_reports(report):
+    for i in range(len(report)):
+        if is_safe_decreasing(report[:i] + report[i + 1 :]):
+            return True
+    return False
+
+
+def get_safe_reports(reports):
+    safe_increasing_reports = []
+    safe_decreasing_reports = []
+
+    for report in reports:
+        if is_safe_increasing(report):
+            safe_increasing_reports.append(report)
+        elif safe_increasing_dampened_reports(report):
+            safe_increasing_reports.append(report)
+
+        if is_safe_decreasing(report):
+            safe_decreasing_reports.append(report)
+        elif safe_decreasing_dampened_reports(report):
+            safe_decreasing_reports.append(report)
+
+    return safe_increasing_reports + safe_decreasing_reports
+
+
+get_safe_reports(get_reports())
